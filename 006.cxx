48ab3056a274be81ddbf9fb2d7b64b2c887afd24
Konstantin Mandrika <dr.fake@gmail.com>
2010-12-15 20:25:12 -0500

    solution to problem 6

---
+++ 006.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char* argv[])
+{
+    unsigned long long sum_of_numbers = 0;
+    unsigned long long sum_of_squares = 0;
+
+    for (unsigned int i = 1; i <= 100; ++i) {
+	sum_of_numbers += i;
+	sum_of_squares += pow(i, 2);
+    }
+
+    //! should probably use abs()
+    unsigned long long difference = pow(sum_of_numbers, 2) - sum_of_squares;
+
+    std::cout<<difference<<std::endl;
+}
