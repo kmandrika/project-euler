0e01d6246b5c15f3d5bc88de66de79a260829a57
Konstantin Mandrika <dr.fake@gmail.com>
2010-12-17 14:48:37 -0500

    solution to problem 14

---
+++ 014.cxx
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <utility>
+
+unsigned int next_term(unsigned int previous_term, bool odd)
+{
+        return odd ? 3 * previous_term + 1 : previous_term / 2;
+}
+
+unsigned int find_longest_chain(unsigned int upper_bound)
+{
+        std::pair<unsigned int, unsigned int> highest = std::make_pair(
+                0     // chain length
+	      , 0     // the number that produced this length
+        );
+
+        while (upper_bound != 1) {
+	        unsigned int chain_length = 0;
+		unsigned int term = upper_bound;
+
+		while ((term = next_term(term, term % 2 != 0)) != 1)
+		        ++chain_length;
+
+		if (chain_length > highest.first) {
+		        highest.first = chain_length;
+			highest.second = upper_bound;
+		}
+
+		--upper_bound;
+	}
+
+	return highest.second;
+}
+
+int main(int argc, char* argv[])
+{
+        std::cout<<find_longest_chain(999999)<<std::endl;
+}
