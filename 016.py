21feeeb145295eeabc6c547a904ffdf7220cfd72
Konstantin Mandrika <dr.fake@gmail.com>
2010-12-22 10:58:54 -0500

    fixing a stupid typo

--- 016.py
+++ 016.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python/python2.4
 
 def digit_sum(number):
-    return reduce(lambda x, y: int(x) + int(y), str(2 ** 1000))
+    return reduce(lambda x, y: int(x) + int(y), str(number))
 
 if __name__ == '__main__':
     print digit_sum(2 ** 1000)
