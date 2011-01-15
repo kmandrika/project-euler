19aa0bb7065d3b2ec098ecb046f9edfa7a7d2cc5
Konstantin Mandrika <dr.fake@gmail.com>
2011-01-15 18:47:39 -0500

    solution to problem 71

---
+++ problem071/solution.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+
+unsigned int problem71()
+{
+        return static_cast<long double>(3) / 7 * 1000000 - 1;
+}
+
+int main(int argc, char* argv[])
+{
+        std::cout<<problem71()<<std::endl;
+}
